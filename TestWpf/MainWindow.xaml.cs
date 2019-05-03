fa4a461cfbf59e33eb81e7d208e12dbe8763b6bc
valkamuz <valkamuz@yandex.ru>
2019-05-04 00:57:27 +0500

    Реализованы первые наброски приложения - калькулятор
    	new file:   .gitignore
    	new file:   TestWpf.sln
    	new file:   TestWpf/App.config
    	new file:   TestWpf/App.xaml
    	new file:   TestWpf/App.xaml.cs
    	new file:   TestWpf/MainWindow.xaml
    	new file:   TestWpf/MainWindow.xaml.cs
    	new file:   TestWpf/Properties/AssemblyInfo.cs
    	new file:   TestWpf/Properties/Resources.Designer.cs
    	new file:   TestWpf/Properties/Resources.resx
    	new file:   TestWpf/Properties/Settings.Designer.cs
    	new file:   TestWpf/Properties/Settings.settings
    	new file:   TestWpf/TestWpf.csproj

---
+++ TestWpf/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string leftop       = "";   // Левый операнд
+        string operation    = "";   // Знак операции
+        string rightop      = "";   // Правый операнд
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            // Добавляем обработчик для всех кнопок на гриде
+            foreach (UIElement c in LayoutRoot.Children)
+            {
+                if (c is Button)
+                {
+                    ((Button)c).Click += Button_Click;
+                }
+            }
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Получаем текст кнопки и добавляем его в текстовое поле
+            string buttonText = (string)((Button) e.OriginalSource).Content;
+            textBlock.Text += buttonText;
+
+            // Если текст - это число
+            if (Int32.TryParse(buttonText, out int num))
+            {
+                // Если операция не задана
+                if (operation == "")
+                {
+                    // Добавляем к левому операнду
+                    leftop += buttonText;
+                }
+                else
+                {
+                    // Иначе к правому операнду
+                    rightop += buttonText;
+                }
+            }
+            // Если было введено не число
+            else
+            {
+                // Если равно, то выводим результат операции
+                if (buttonText == "=")
+                {
+                    Update_RightOp();
+                    textBlock.Text += rightop;
+                    operation = "";
+                }
+                // Очищаем поле и переменные
+                else if (buttonText == "CLEAR")
+                {
+                    leftop = "";
+                    rightop = "";
+                    operation = "";
+                    textBlock.Text = "";
+                }
+                // Получаем операцию
+                else
+                {
+                    // Если правый операнд уже имеется, то присваиваем его значение левому
+                    // операнду, а правый операнд очищаем
+                    if (rightop != "")
+                    {
+                        Update_RightOp();
+                        leftop = rightop;
+                        rightop = "";
+                    }
+                    operation = buttonText;
+                }
+            }
+        }
+
+
+        // Обновляем значение правого операнда
+        private void Update_RightOp()
+        {
+            int num1 = Int32.Parse(leftop);
+            int num2 = Int32.Parse(rightop);
+            // И выполняем операцию
+            switch (operation)
+            {
+                case "+":
+                    rightop = (num1 + num2).ToString();
+                    break;
+                case "-":
+                    rightop = (num1 - num2).ToString();
+                    break;
+                case "*":
+                    rightop = (num1 * num2).ToString();
+                    break;
+                case "/":
+                    rightop = (num1 / num2).ToString();
+                    break;
+            }
+        }
+    }
+}
